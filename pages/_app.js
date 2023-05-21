c74ad160b7fcfe389ac1e0e1f606a977929a99b7
Gyanendra Yadav <49138951+iGyanendraYadav@users.noreply.github.com>
2023-05-21 08:43:50 +0530

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,11 +1,13 @@
 import '../styles/globals.css'
 import Footer from '../components/footer'
 import Navbar from '../components/navbar'
+import ScrollToTop from '../components/scrolltotop'
 
 function MyApp({ Component, pageProps }) {
   return <> 
             <Navbar />
             <Component {...pageProps} />
+             <ScrollToTop/>
             <Footer/>
           </>
 }
